e7e9362cbe114f72fd971da6ac30a1aa3fb9396f
zagzam <zagzam@yahoo.com>
2021-03-31 10:09:23 +0800

    hparams edited

--- hparams.py
+++ hparams.py
@@ -78,10 +78,10 @@ def create_hparams(hparams_string=None, verbose=False):
         # Optimization Hyperparameters #
         ################################
         use_saved_learning_rate=False,
-        learning_rate=1e-4,
+        learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=32,
+        batch_size=40,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
